0196a2c064eac8d216a20b7df5b593dfa513da90
Marjorie9 <marjoriekatedeguzman@gmail.com>
2024-11-04 18:33:09 +0800

    userProfileObject.js

--- userProfileObject.js
+++ userProfileObject.js
@@ -1,8 +1,8 @@
 const userProfile = {
     fullName: "Marjorie Kate De guzman",
     age: 19, 
-    favoriteNumber: 17,     // Replace with the user's favorite number
-    favoriteColors: ["blue", "yellow", "white"] // Replace with the user's favorite colors
+    favoriteNumber: 17,
+    favoriteColors: ["blue", "yellow", "white"]
 };
 
 console.log(userProfile);
